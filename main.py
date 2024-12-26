70bc8b66082caa307fbd59f79d348b7d6077d5fe
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-26 20:23:55 +0100

    This took a while, but I figured out how to change the color and how to make things resizable.
    Also added a license.

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
+from kivy.uix.behaviors.touchripple import Color
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.clock import Clock
 from kivy.uix.effectwidget import Rectangle
+
+gridWidth = 15
+gridHeight = 25
 
 # the class of the main grid
 class MainGrid(GridLayout):
     def setup(self):
         print("setup")
         self.gameGrid = []
-        for i in range(30):
+        for i in range(gridHeight):
             self.gameGrid.append([])
-            for j in range(10):
+            for j in range(gridWidth):
                 self.gameGrid[i].append(0)
+        self.changeSize()
+        # this is just for testing
+        self.gameGrid[0][0] = 1
+        self.gameGrid[2][2] = 1
+        self.clockEvent = Clock.schedule_interval(self.update, 1)
+    def changeSize(self):
+        self.sizeTile = self.size[1] / (gridHeight + gridHeight/5)
+        print("size changed to", self.sizeTile)
     def draw(self):
         print("draw")
         self.ids.tetrisCanvas.canvas.clear()
-        #self.ids.tetrisCanvas.canvas.add(Rectangle(pos=(0,0), size=(100,100)))
-    def start(self):
-        print("start")
-        self.clockEvent = Clock.schedule_interval(self.update, 1)
+        for i in range(gridHeight):
+            for j in range(gridWidth):
+                if self.gameGrid[i][j] == 0:
+                    #print("drawing white")
+                    self.ids.tetrisCanvas.canvas.add(Color(rgba=(1, 1, 1, 1)))
+                    self.ids.tetrisCanvas.canvas.add(Rectangle(pos=(j*self.sizeTile+self.size[0]/2-self.sizeTile*gridWidth/2, i*self.sizeTile), size = (self.sizeTile, self.sizeTile)))
+                else:
+                    print("drawing red")
+                    self.ids.tetrisCanvas.canvas.add(Color(rgba=(1, 0, 0, 1)))
+                    self.ids.tetrisCanvas.canvas.add(Rectangle(pos=(j*self.sizeTile+self.size[0]/2-self.sizeTile*gridWidth/2, i*self.sizeTile), size =(self.sizeTile, self.sizeTile)))
     def update(self, dt):
-        print("update")
+        #print("update")
         # some moving logic will be here
         self.draw()
     def settings(self):
