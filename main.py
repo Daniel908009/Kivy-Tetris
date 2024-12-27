e8a978adbfe40230454ca7fcb2eeca13bb1587ce
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-27 15:39:53 +0100

    there is a lot of problems with the movement, I am trying to figure it out.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.clock import Clock
 from kivy.uix.effectwidget import Rectangle
+from kivy.core.window import Window
 import random
 
 gridWidth = 16
@@ -14,15 +15,70 @@ objectList = [[[int(gridWidth/2),gridHeight-1],[int(gridWidth/2),gridHeight-2],[
 [[int(gridWidth/2),gridHeight-1],[int(gridWidth/2),gridHeight-2],[int(gridWidth/2)-1,gridHeight-2],[int(gridWidth/2)-1,gridHeight-1]], # square
 [[int(gridWidth/2),gridHeight-1],[int(gridWidth/2),gridHeight-2],[int(gridWidth/2)+1,gridHeight-2],[int(gridWidth/2)+1,gridHeight-3]], # rotated z
 [[int(gridWidth/2),gridHeight-1],[int(gridWidth/2),gridHeight-2],[int(gridWidth/2)+1,gridHeight-2],[int(gridWidth/2)+1,gridHeight-3]], # rotated s
-[[int(gridWidth/2),gridHeight-1],[int(gridWidth/2),gridHeight-2],[int(gridWidth/2),gridHeight-3],[int(gridWidth/2)+1,gridHeight-3]], # L right
+[[int(gridWidth/2),gridHeight-1],[int(gridWidth/2),gridHeight-2],[int(gridWidth/2)+1,gridHeight-3],[int(gridWidth/2),gridHeight-3]], # L right
 [[int(gridWidth/2),gridHeight-1],[int(gridWidth/2),gridHeight-2],[int(gridWidth/2),gridHeight-3],[int(gridWidth/2)-1,gridHeight-2]]] # T
 
 # the class of the main grid
 class MainGrid(GridLayout):
+    def __init__(self):
+        super(MainGrid, self).__init__()
+        Window.bind(on_key_down=self.keyAction)
+    def keyAction(self, instance, keyboard, keycode, text, modifiers):
+        #print(text)
+        if text == "a":
+            self.moveObject("left")
+        elif text == "d":
+            self.moveObject("right")
+        elif text == "s":
+            self.moveObject("down")
+        elif text == "w":
+            self.moveObject("rotate")
+    def moveObject(self, direction):
+        print(self.moveTiles)
+        tilesToRemove = []
+        tilesToAppend = []
+        if direction == "left" and self.lastMove != "left":
+            for tile in self.moveTiles:
+                if tile[1] <= 0 or [tile[0], tile[1]-1] in self.staticTiles:
+                    return
+            for tile in self.moveTiles:
+                self.gameGrid[tile[0]][tile[1]] = 0
+                self.gameGrid[tile[0]][tile[1]-1] = 1
+                tilesToRemove.append([tile[0], tile[1]]) # tiles have to be removed outside of this loop since otherwise it will start skipping tiles
+                tilesToAppend.append([tile[0], tile[1]-1])
+            self.lastMove = "left"
+            #print(self.moveTiles, "changed to left")
+        elif direction == "right" and self.lastMove != "right":
+            for tile in self.moveTiles:
+                if tile[1] >= gridWidth-1 or [tile[0], tile[1]+1] in self.staticTiles:
+                    return
+            for tile in self.moveTiles:
+                self.gameGrid[tile[0]][tile[1]] = 0
+                self.gameGrid[tile[0]][tile[1]+1] = 1
+                tilesToRemove.append([tile[0], tile[1]])
+                tilesToAppend.append([tile[0], tile[1]+1])
+            self.lastMove = "right"
+        elif direction == "down" and self.lastMove != "down":
+            for tile in self.moveTiles:
+                if tile[0] <= 0 or [tile[0]-1, tile[1]] in self.staticTiles:
+                    return
+            for tile in self.moveTiles:
+                self.gameGrid[tile[0]][tile[1]] = 0
+                self.gameGrid[tile[0]-1][tile[1]] = 1
+                tilesToRemove.append([tile[0], tile[1]])
+                tilesToAppend.append([tile[0]-1, tile[1]])
+            self.lastMove = "down"
+        elif direction == "rotate" and self.lastMove != "rotate":
+            pass # I have absolutely no idea how to rotate the objects, so I will leave this empty for now
+        for tile in tilesToRemove:
+            self.moveTiles.remove(tile)
+        for tile in tilesToAppend:
+            self.moveTiles.append(tile)
     def setup(self):
         self.gameGrid = []
         self.moveTiles = []
         self.staticTiles = []
+        self.lastMove = ""
         for i in range(gridHeight):
             self.gameGrid.append([])
             for j in range(gridWidth):
@@ -49,14 +105,11 @@ class MainGrid(GridLayout):
                 else:
                     self.ids.tetrisCanvas.canvas.add(Color(rgba=(1, 0, 0, 1)))
                     self.ids.tetrisCanvas.canvas.add(Rectangle(pos=(j*self.sizeTile+self.size[0]/2-self.sizeTile*gridWidth/2, i*self.sizeTile), size =(self.sizeTile, self.sizeTile)))
+        self.lastMove = ""
     def spawnObject(self):
         if len(self.moveTiles) == 0:
             self.choice = random.choice(objectList)
-            #print(self.choice)
             for tile in self.choice:
-                #print(tile[1])
-                #print(tile[0])
-                #print(len(self.gameGrid))
                 if self.gameGrid[tile[1]][tile[0]] == 1:
                     self.gameOver()
                     break
@@ -72,7 +125,6 @@ class MainGrid(GridLayout):
         self.spawnObject()
         self.checkLines()
     def move(self):
-        #print(self.moveTiles)
         for i in range(gridHeight):
             for j in range(gridWidth):
                 if self.gameGrid[i][j] == 1 and [i, j] in self.moveTiles:
@@ -83,19 +135,12 @@ class MainGrid(GridLayout):
                         self.moveTiles = []
         for i in range(gridHeight):
             for j in range(gridWidth):
-                #if self.gameGrid[i][j] == 1:
-                #    print(str([i, j]) + " is the tile")
-                #    print("this condition works")
-                #if [i, j] in self.moveTiles:
-                    #print("this condition also works ----------------")
                 if self.gameGrid[i][j] == 1 and [i, j] in self.moveTiles:
-                    #print("works here")
                     if [i-1, j] not in self.staticTiles:
                         self.gameGrid[i][j] = 0
                         self.gameGrid[i-1][j] = 1
                         self.moveTiles.remove([i, j])
                         self.moveTiles.append([i-1, j])
-        #print(self.gameGrid)
     def checkLines(self):
         for i in range(gridHeight):
             if 0 not in self.gameGrid[i]:
